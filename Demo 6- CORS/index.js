dd2921d64a70672099bcb6af2e20b0c03116f902
arunrajora <rajora.rajora2@outlook.com>
2017-04-10 14:44:16 +0530

    add cors demo.

--- Demo 6- CORS/index.js
+++ Demo 6- CORS/index.js
@@ -2,29 +2,43 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , bodyParser = require('body-parser')
-  , app = express()
+  , app1 = express()
+  , app2 = express()
   , morgan = require('morgan');
 
-  app.set('port', 3000);
-  app.use(morgan(':method :url Status :status :req[body] - :response-time ms'));
-  app.use(bodyParser.urlencoded({
+  app1.set('port', 3000);
+  app1.use(morgan(':method :url Status :status :req[body] - :response-time ms'));
+  app1.use(bodyParser.urlencoded({
     extended: true
   }));
-  app.use(express.static(path.join(__dirname, 'public')));
+  app1.use(express.static(path.join(__dirname, 'public')));
+
+  app2.set('port', 3005);
+  app2.use(morgan(':method :url Status :status :req[body] - :response-time ms'));
+  app2.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app2.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/sample-get',function(req,res){
-    res.send(Math.random().toString());
+/*app2.options("/randomnumber", function(req, res){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, authentication');
+  res.send(200);
+});*/
+
+app2.get('/randomnumber',function(req,res){
+  /*res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, authentication');*/
+    res.send((Math.floor(Math.random()*10)+1).toString());
 });
 
-app.post('/sample-post', function(req, res) {
-    var fname = req.body.fname,
-        lname = req.body.lname,
-        age = req.body.age;
-        console.log("Post received: ",res.body);
-        res.send("Hi "+fname+" "+lname+"\n"+"Thanks for signing up.");
+http.createServer(app1).listen(app1.get('port'), function(){
+  console.log("Express server listening on port " + app1.get('port'));
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+http.createServer(app2).listen(app2.get('port'), function(){
+  console.log("Express server listening on port " + app2.get('port'));
 });
